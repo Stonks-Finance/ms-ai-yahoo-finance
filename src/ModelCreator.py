4bbe70f2947693e288a315ed9293f2ab6ced75ad
recabet <rajab.iskenderli@gmail.com>
2024-10-26 15:59:57 +0400

    Changes to ModelCreator.py filepaths and implemented fmt changer

--- src/ModelCreator.py
+++ src/ModelCreator.py
@@ -1,26 +1,23 @@
-import os
 import keras
 from src.Tuner import Tuner
-from typing import List,Dict
+from typing import List, Dict
 
 
 class ModelCreator(Tuner):
     def __init__ (self,
-                  project_name: str,
                   stock_symbol: str,
-                  fmt:str,
+                  fmt: str,
                   intervals: List[str],
                   max_trials: int = 10,
                   executions_per_trial: int = 3,
-                  directory: str = "tuner_dir",
-                  )-> None:
+                  directory: str = "tuning_histories",
+                  ) -> None:
         
-        super().__init__(project_name,
-                         fmt,
+        super().__init__(fmt,
                          max_trials,
                          executions_per_trial,
                          directory)
-
+        
         self.stock_symbol = stock_symbol
         self.intervals = intervals
         self.models = None
@@ -30,14 +27,14 @@ class ModelCreator(Tuner):
                     batch_size: int = 32,
                     metric: str = "val_loss",
                     plot: bool = False,
-                    verbose: bool = True)->Dict[str, keras.Model]:
+                    verbose: bool = True) -> Dict[str, keras.Model]:
         
-        models:Dict[str,keras.Model] = {}
+        models: Dict[str, keras.Model] = {}
         for interval in self.intervals:
-            model_name = f"{interval}_{self.stock_symbol}_model"
+            model_name: str = f"{interval}_{self.stock_symbol}_model"
             print(f"Tuning for interval: {interval}")
             
-            models[model_name] = self.tune(self.stock_symbol,
+            models[model_name] = self.__tune(self.stock_symbol,
                                            interval,
                                            epochs,
                                            batch_size,
@@ -47,14 +44,3 @@ class ModelCreator(Tuner):
         
         self.models = models
         return models
-    
-    def save_models (self, directory: str)->None:
-        if not self.models:
-            raise ValueError("No models to save. Please run `train_tune()` first.")
-        
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        
-        for model_name, model in self.models.items():
-            model.save(os.path.join(directory, f"{model_name}{self.fmt}"))
-            print(f"Model {model_name} saved.")
