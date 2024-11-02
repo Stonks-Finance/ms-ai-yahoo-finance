b7ce0a3af5a3004071fff556b7457115c10df215
recabet <rajab.iskenderli@gmail.com>
2024-11-02 15:26:31 +0400

    Used Enum instead of dict

--- api/endpoints/past_values.py
+++ api/endpoints/past_values.py
@@ -4,8 +4,10 @@ from pydantic import BaseModel
 from typing import List, Dict
 import yfinance as yf
 from dateutil.relativedelta import relativedelta
+from api.Enums.Duration import MaxDurationLimit
 
 router = APIRouter()
+
 
 class PastValuesResponse(BaseModel):
     success: bool
@@ -13,40 +15,42 @@ class PastValuesResponse(BaseModel):
     message: str
     data: Dict[str, List]
 
-MAX_DURATION_LIMIT = {
-    "1d": 1500,
-    "1mo": 120
-}
 
-def fetch_past_stock_data(stock_name: str, interval: str, duration: int) -> Dict[str, List]:
+def fetch_past_stock_data (stock_name: str, interval: str, duration: int) -> Dict[str, List]:
     end_date = datetime.datetime.now()
-
+    start_date = None
+    
     if interval == "1d":
         start_date = end_date - datetime.timedelta(days=duration)
     elif interval == "1mo":
         start_date = end_date - relativedelta(months=duration - 1)
-
+    
     stock_data = yf.download(stock_name, start=start_date, end=end_date, interval=interval)
-
+    
     if stock_data.empty:
         raise ValueError(f"No data found for stock '{stock_name}'.")
-
-    prices = stock_data['Adj Close'].values.flatten().tolist()
+    
+    prices = stock_data["Adj Close"].values.flatten().tolist()
     timestamps = [date.strftime("%Y-%m-%dT%H:%M:%S") for date in stock_data.index]
-
+    
     return {"prices": prices, "timestamps": timestamps}
 
-@router.get("/past-values", response_model=PastValuesResponse)
-async def past_values(stock_name: str, interval: str = Query("1d", enum=["1d", "1mo"]), duration: int = Query(7)):
 
-    if duration > MAX_DURATION_LIMIT.get(interval, float('inf')):
+@router.get("/past-values", response_model=PastValuesResponse)
+async def past_values (
+        stock_name: str,
+        interval: str = Query("1d", enum=[d.interval for d in MaxDurationLimit]),
+        duration: int = Query(7)
+):
+    max_duration = next((d.limit for d in MaxDurationLimit if d.interval == interval), float('inf'))
+    if duration > max_duration:
         return {
             "success": False,
             "status": 400,
-            "message": f"Duration for interval '{interval}' cannot exceed {MAX_DURATION_LIMIT[interval]}.",
+            "message": f"Duration for interval '{interval}' cannot exceed {max_duration}.",
             "data": {"prices": [], "timestamps": []}
         }
-
+    
     try:
         past_data = fetch_past_stock_data(stock_name, interval, duration)
         return {
@@ -69,4 +73,3 @@ async def past_values(stock_name: str, interval: str = Query("1d", enum=["1d", "
             "message": "Internal server error: " + str(e),
             "data": {"prices": [], "timestamps": []}
         }
-
