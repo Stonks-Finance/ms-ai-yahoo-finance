f707131bf077aba2f09cbf495d081f508527e086
recabet <rajab.iskenderli@gmail.com>
2024-11-02 15:27:05 +0400

    Removed the Seed

--- create_models/create_AAPL_models.py
+++ create_models/create_AAPL_models.py
@@ -1,9 +1,5 @@
 from src.Classes.ModelCreator import ModelCreator
-from src.SEEDS import set_seed
 
-SEED:int=6
-set_seed(SEED)
-
-creator=ModelCreator("AAPL",".h5",["1m","1h"])
+creator = ModelCreator("AAPL", ".h5", ["1m", "1h"])
 
 creator.train_tune(plot=True)
