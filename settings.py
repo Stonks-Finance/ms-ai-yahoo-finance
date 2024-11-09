338fed7864152849af4ddea71b4e8dc883ecba84
recabet <rajab.iskenderli@gmail.com>
2024-11-09 20:19:19 +0400

    added CREATE_MODELS to settings.py

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 _TUNING_HISTORIES_DIR=str(os.getenv("_TUNING_HISTORIES_DIR"))
 _MODELS_DIR=str(os.getenv("_MODELS_DIR"))
+CREATE_MODELS_DIR:str="../create_models"
 
 MODELS_DIRECTORY = os.path.join(BASE_DIR, _MODELS_DIR)
 TUNING_HISTORIES_DIRECTORY = os.path.join(BASE_DIR, _TUNING_HISTORIES_DIR)
