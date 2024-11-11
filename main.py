74d5d7bea81bdda9b7c96bd65221a04ee84ff243
recabet <rajab.iskenderli@gmail.com>
2024-11-11 22:35:29 +0400

    Added multithreading

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import threading
 import uvicorn
+from src.automate_training import run_schedule
+import os
+from settings import DEBUG
+
+if DEBUG:
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = '1'
+else:
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'
+
+def run_api():
+    uvicorn.run("api.api:api", host="127.0.0.1", port=8000, reload=False)
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "api.api:api",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
-    )
+    api_thread = threading.Thread(target=run_api)
+    api_thread.start()
+
+    schedule_thread = threading.Thread(target=run_schedule)
+    schedule_thread.start()
+
+    api_thread.join()
+    schedule_thread.join()
