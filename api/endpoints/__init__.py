0c61af91d23f5e31a5933ca4d89dd66ae3238a17
recabet <rajab.iskenderli@gmail.com>
2024-11-11 22:37:09 +0400

    formatting changes

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -1,8 +1,8 @@
-# __init__.py
 from .predict import router as predict_router
 from .stock_overview import router as stock_overview_router
 from .historical_data import router as historical_data_router
 from .past_values import router as past_values_router
+
 
 __all__ = [
     "predict_router",
