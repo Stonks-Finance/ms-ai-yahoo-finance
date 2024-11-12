4c8e748bc68eae66130174ea91a62326f991fa5b
recabet <rajab.iskenderli@gmail.com>
2024-11-12 18:25:32 +0400

    New thread for refittng

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import threading
 import uvicorn
-from src.automate_training import run_schedule
+from src.automate_training import run_schedule,run_refit_schedule
 import os
-from settings import DEBUG
+from settings import DEBUG,CREATE_MODELS_DIR
 
 if DEBUG:
     os.environ["TF_CPP_MIN_LOG_LEVEL"] = '1'
@@ -13,11 +13,16 @@ def run_api():
     uvicorn.run("api.api:api", host="127.0.0.1", port=8000, reload=False)
 
 if __name__ == "__main__":
+
     api_thread = threading.Thread(target=run_api)
     api_thread.start()
-
-    schedule_thread = threading.Thread(target=run_schedule)
-    schedule_thread.start()
+    
+    create_thread = threading.Thread(target=run_schedule, args=(30, CREATE_MODELS_DIR))
+    create_thread.start()
+    
+    refit_thread = threading.Thread(target=run_refit_schedule, args=(15, CREATE_MODELS_DIR))
+    refit_thread.start()
 
     api_thread.join()
-    schedule_thread.join()
+    create_thread.join()
+    refit_thread.join()
