124ca5a868100bbb35d3f44d81ce1f20076aa4a9
recabet <rajab.iskenderli@gmail.com>
2024-11-12 18:24:39 +0400

    ModelCreator now creates 1 model at a time,and saes hps

--- src/Classes/ModelCreator.py
+++ src/Classes/ModelCreator.py
@@ -1,14 +1,14 @@
 import os
 import keras
 from src.Classes.Tuner import Tuner
-from typing import List, Dict
+from typing import  Dict,Optional
 from settings import TUNING_HISTORIES_DIRECTORY
 
 
 class ModelCreator(Tuner):
     def __init__ (self,
                   stock_symbol: str,
-                  intervals: List[str],
+                  interval: str,
                   max_trials: int = 10,
                   executions_per_trial: int = 3,
                   directory: str = TUNING_HISTORIES_DIRECTORY,
@@ -25,28 +25,33 @@ class ModelCreator(Tuner):
             models_dir)
         
         self.stock_symbol = stock_symbol
-        self.intervals = intervals
-        self.models = models_dir
+        self.interval = interval
+        self.models_dir = models_dir
+        self.model = None
     
     def train_tune (self,
                     epochs: int = 10,
                     batch_size: int = 32,
                     metric: str = "val_loss",
                     plot: bool = False,
-                    verbose: bool = True) -> Dict[str, keras.Model]:
+                    verbose: bool = True) -> keras.Model:
         
-        models: Dict[str, keras.Model] = {}
-        for interval in self.intervals:
-            model_name: str = f"{interval}_{self.stock_symbol}_model"
-            print(f"Tuning for interval: {interval}")
-            
-            models[model_name] = self._Tuner__tune(self.stock_symbol,
-                                                   interval,
-                                                   epochs,
-                                                   batch_size,
-                                                   metric,
-                                                   plot,
-                                                   verbose)
+        print(f"Tuning for interval: {self.interval}")
         
-        self.models = models
-        return models
+        model = self._Tuner__tune(self.stock_symbol,
+                                  self.interval,
+                                  epochs,
+                                  batch_size,
+                                  metric,
+                                  plot,
+                                  verbose)
+        
+        self.model = model
+        return model
+    
+    def get_hps (self) -> Optional[Dict[str, any]]:
+        
+        if self.hps:
+            return {param: self.hps.get(param) for param in self.hps.values}
+        else:
+            raise ValueError("Hyperparameters have not been tuned yet.")
