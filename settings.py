be9bed61ce44963b5c0e2791cf0ccb1c45f96c7c
recabet <rajab.iskenderli@gmail.com>
2024-11-19 01:04:52 +0400

    Updated .env and settings.py

--- settings.py
+++ settings.py
@@ -5,20 +5,16 @@ load_dotenv()
 
 BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
 
-_TUNING_HISTORIES_DIR = str(os.getenv("_TUNING_HISTORIES_DIR"))
-
-_MODELS_DIR = str(os.getenv("_MODELS_DIR"))
-
 CREATE_MODELS_DIR: str = "create_models"
 
-MODELS_DIRECTORY: str = os.path.join(BASE_DIR, _MODELS_DIR)
+MODELS_DIRECTORY: str = os.path.join(BASE_DIR, str(os.getenv("MODELS_DIRECTORY")))
 
-TUNING_HISTORIES_DIRECTORY: str = os.path.join(BASE_DIR, _TUNING_HISTORIES_DIR)
+TUNING_HISTORIES_DIRECTORY: str = os.path.join(BASE_DIR, str(os.getenv("TUNING_HISTORIES_DIRECTORY")))
 
 DEBUG: int = os.getenv("DEBUG", "False").lower() == "true"
 
-SEED: int = int(os.getenv("_SEED", 6))
+SEED: int = int(os.getenv("SEED", 6))
 
-IP: str = os.getenv("_IP", "0.0.0.0")
+IP: str = os.getenv("IP", "0.0.0.0")
 
-PORT: int = int(os.getenv("_PORT", "8000"))
+PORT: int = int(os.getenv("PORT", "8000"))
